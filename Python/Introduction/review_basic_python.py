0df1307ac425f857c08982d532e2de36d1345f2e
Amele21 <melendra@oregonstate.edu>
2023-02-02 16:20:39 -0800

    class and methods intro folder

--- Python/Introduction/review_basic_python.py
+++ Python/Introduction/review_basic_python.py
@@ -1,4 +1,4 @@
-# Sitation
+# Citation
 # Problem Solving with Algorithms and Data Structures using Python
 # By Brad Miller and David Ranum, Luther College
 # https://runestone.academy/ns/books/published/pythonds/index.html
