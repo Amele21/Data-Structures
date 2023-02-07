9cf645d7ec2baacab7971932a89c213cdbe006d9
Amele21 <melendra@oregonstate.edu>
2023-02-07 09:33:33 -0800

    brute force

--- Python/Algorithm_Analysis/anagram_bruteForce.py
+++ Python/Algorithm_Analysis/anagram_bruteForce.py
@@ -2,10 +2,11 @@
 # Solution 3: Brute Force
 # Big-O: O(n!)
 
-def anagramSolution3(s1,s2, i, exist):
+# Recursion approach
+def anagramSolution3a(s1,s2, i, exist):
     length = len(s1)
     data = list(s1)
-   
+    
     if data == list(s2): 
         return True    
     else: 
@@ -14,19 +15,17 @@ def anagramSolution3(s1,s2, i, exist):
             data[i], data[j] = data[j], data[i] 
 
             # recursion
-            rec = anagramSolution3(data, s2, i+1, exist) 
+            rec = anagramSolution3a(data, s2, i+1, exist) 
             if rec == True:
                 exist = True
           
             # backtrack
             data[i], data[j] = data[j], data[i]
 
- 
+        
         return exist
 
 
-
-
-print(anagramSolution3('abc', 'bca', 0, exist=False))
+print(anagramSolution3a('abc', 'bca', 0, exist=False))
 
 
