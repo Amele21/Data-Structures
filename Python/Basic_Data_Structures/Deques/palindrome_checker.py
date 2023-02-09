035bde607c800a877f47b6574fc7151795d57601
Amele21 <melendra@oregonstate.edu>
2023-02-08 17:17:20 -0800

    deques programs

--- Python/Basic_Data_Structures/Deques/palindrome_checker.py
+++ Python/Basic_Data_Structures/Deques/palindrome_checker.py
@@ -3,4 +3,26 @@
 # By Brad Miller and David Ranum, Luther College
 # https://runestone.academy/ns/books/published/pythonds/index.html
 # Meant for pratice and note taking
+from deque_class import Deque
 
+
+def palchecker(aString):
+    chardeque = Deque()
+
+    # add items through rear
+    for ch in aString:
+        chardeque.addRear(ch)
+
+    stillEqual = True
+
+    # remove items from rear and front. Check if they are equal. 
+    while chardeque.size() > 1 and stillEqual:
+        first = chardeque.removeFront()
+        last = chardeque.removeRear()
+        if first != last:
+            stillEqual = False
+
+    return stillEqual
+
+print(palchecker("lsdkjfskf"))
+print(palchecker("radar"))
