b583e7e71afb73a7b0b945c8a3a8a0e25d9ca548
Amele21 <melendra@oregonstate.edu>
2023-02-17 13:16:00 -0800

    selection sort added

---
+++ Python/Searching_and_Sorting/selection_sort.py
@@ -0,0 +1,27 @@
+# Citation
+# Problem Solving with Algorithms and Data Structures using Python
+# By Brad Miller and David Ranum, Luther College
+# https://runestone.academy/ns/books/published/pythonds/index.html
+# Meant for pratice and note taking
+# Sorting
+
+# O(n^2)
+# fillslot is the slot that needs to be filled. 
+# Starting with the highest value.
+# Each loop, it goes down one value
+# The second loop is looking for the positon of the max value
+# lines 21-23 swap the the max value and the value on the max position
+def selectionSort(alist):
+   for fillslot in range(len(alist)-1,0,-1):
+       positionOfMax=0
+       for location in range(1,fillslot+1):
+           if alist[location]>alist[positionOfMax]:
+               positionOfMax = location
+
+       temp = alist[fillslot]
+       alist[fillslot] = alist[positionOfMax]
+       alist[positionOfMax] = temp
+
+alist = [54,26,93,17,77,31,44,55,20]
+selectionSort(alist)
+print(alist)
