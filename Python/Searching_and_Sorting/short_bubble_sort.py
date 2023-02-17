9b4cbcca699014015092b250e0b069657c7f36b5
Amele21 <melendra@oregonstate.edu>
2023-02-17 12:15:28 -0800

    added short bubble sort

---
+++ Python/Searching_and_Sorting/short_bubble_sort.py
@@ -0,0 +1,27 @@
+# Citation
+# Problem Solving with Algorithms and Data Structures using Python
+# By Brad Miller and David Ranum, Luther College
+# https://runestone.academy/ns/books/published/pythonds/index.html
+# Meant for pratice and note taking
+# Sorting
+
+# O(n^2)
+# Modified bubble sort that that recognizes a sorted list and stops
+# This is done through excahnges. If exchanges remains False through the second loop
+# Program ends since that indicates the list is sorted. 
+def shortBubbleSort(alist):
+    exchanges = True
+    passnum = len(alist)-1
+    while passnum > 0 and exchanges:
+       exchanges = False
+       for i in range(passnum):
+           if alist[i]>alist[i+1]:
+               exchanges = True
+               temp = alist[i]
+               alist[i] = alist[i+1]
+               alist[i+1] = temp
+       passnum = passnum-1
+
+alist=[20,30,40,90,50,60,70,80,100,110]
+shortBubbleSort(alist)
+print(alist)
