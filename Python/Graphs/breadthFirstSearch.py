46081bd8ce5ada1d64c01b5293ac44ac019c9f9b
Amele21 <melendra@oregonstate.edu>
2023-03-02 16:54:24 -0800

    added bfs

---
+++ Python/Graphs/breadthFirstSearch.py
@@ -0,0 +1,26 @@
+# Citation
+# Problem Solving with Algorithms and Data Structures using Python
+# By Brad Miller and David Ranum, Luther College
+# https://runestone.academy/ns/books/published/pythonds/index.html
+# Meant for pratice and note taking
+# Graphs
+
+
+# Breadth First Search
+
+
+
+def bfs(g,start):
+  start.setDistance(0)
+  start.setPred(None)
+  vertQueue = Queue()
+  vertQueue.enqueue(start)
+  while (vertQueue.size() > 0):
+    currentVert = vertQueue.dequeue()
+    for nbr in currentVert.getConnections():
+      if (nbr.getColor() == 'white'):
+        nbr.setColor('gray')
+        nbr.setDistance(currentVert.getDistance() + 1)
+        nbr.setPred(currentVert)
+        vertQueue.enqueue(nbr)
+    currentVert.setColor('black')
